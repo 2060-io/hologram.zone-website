8fdf574c6a68a8671889e35dfb32b1ac02791deb
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-09-16 18:44:38 -0500

    fix: update lint

--- app/app/components/header.tsx
+++ app/app/components/header.tsx
@@ -1,65 +1,66 @@
-import React from "react"
-import Image from "next/image";
-import { Translations } from '../page';
+import Image from 'next/image'
+import React from 'react'
+
+import { Translations } from '../components/utils'
 
 interface Header {
-	translations: Translations
+  translations: Translations
 }
 
-const Header: React.FC<Header> = ({translations}) => {
-	const headerTitleLine1: string[] = (translations.header_title_line_1 ?? '').split('-');
-	const headerTitleLine2: string[] = (translations.header_title_line_2 ?? '').split('-');
-	const headerTitleLine3: string[] = (translations.header_title_line_3 ?? '').split('-');
+const Header: React.FC<Header> = ({ translations }) => {
+  const headerTitleLine1: string[] = (translations.header_title_line_1 ?? '').split('-')
+  const headerTitleLine2: string[] = (translations.header_title_line_2 ?? '').split('-')
+  const headerTitleLine3: string[] = (translations.header_title_line_3 ?? '').split('-')
 
-	return (
-		<header className="pt-4">
-			<div className="flex justify-between mb-5">
-				<Image
-					src="images/logo-hologram.png"
-					alt="Logo"
-					width={150}
-					height={32}
-					className="w-[150px] h-[32px]"
-					priority={true}
-				/>
-				<Image
-					src="images/logo-github.svg"
-					alt="Logo-gitHub"
-					width={32}
-					height={32}
-					id="logo-github-white"
-					className="w-8 h-8"
-					priority={false}
-				/>
-				<Image
-					src="images/logo-github-white.svg"
-					alt="Logo-gitHub"
-					width={32}
-					height={32}
-					id="logo-github-black"
-					className="w-8 h-8"
-					priority={false}
-				/>
-			</div>
-			<div className="mb-4 mt-3 text-2xl md:text-5xl lg:text-6xl font-semibold">
-				<p>
-					<span className="text-hologram-color">{headerTitleLine1[0]}</span>
-					&nbsp;{headerTitleLine1[1]}
-					<span className="text-hologram-color">{translations.dot}</span>
-				</p>
-				<p>
-					<span className="text-hologram-color">{headerTitleLine2[0]}</span>
-					&nbsp;{headerTitleLine2[1]}
-					<span className="text-hologram-color">{translations.dot}</span>
-				</p>
-				<p>
-					<span className="text-hologram-color">{headerTitleLine3[0]}</span>
-					&nbsp;{headerTitleLine3[1]}
-					<span className="text-hologram-color">{translations.dot}</span>
-				</p>
-			</div>
-		</header>
-	)
+  return (
+    <header className="pt-4">
+      <div className="flex justify-between mb-5">
+        <Image
+          src="images/logo-hologram.png"
+          alt="Logo"
+          width={150}
+          height={32}
+          className="w-[150px] h-[32px]"
+          priority={true}
+        />
+        <Image
+          src="images/logo-github.svg"
+          alt="Logo-gitHub"
+          width={32}
+          height={32}
+          id="logo-github-white"
+          className="w-8 h-8"
+          priority={false}
+        />
+        <Image
+          src="images/logo-github-white.svg"
+          alt="Logo-gitHub"
+          width={32}
+          height={32}
+          id="logo-github-black"
+          className="w-8 h-8"
+          priority={false}
+        />
+      </div>
+      <div className="mb-4 mt-3 text-2xl md:text-5xl lg:text-6xl font-semibold">
+        <p>
+          <span className="text-hologram-color">{headerTitleLine1[0]}</span>
+          &nbsp;{headerTitleLine1[1]}
+          <span className="text-hologram-color">{translations.dot}</span>
+        </p>
+        <p>
+          <span className="text-hologram-color">{headerTitleLine2[0]}</span>
+          &nbsp;{headerTitleLine2[1]}
+          <span className="text-hologram-color">{translations.dot}</span>
+        </p>
+        <p>
+          <span className="text-hologram-color">{headerTitleLine3[0]}</span>
+          &nbsp;{headerTitleLine3[1]}
+          <span className="text-hologram-color">{translations.dot}</span>
+        </p>
+      </div>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
--- app/app/components/sections/sectionWhatIs.tsx
+++ app/app/components/sections/sectionWhatIs.tsx
@@ -1,28 +1,29 @@
-import React from "react";
-import { Translations } from "@/app/page";
-import Image from "next/image";
+import Image from 'next/image'
+import React from 'react'
+
+import { Translations } from '../utils'
 
 interface SectionWhatIs {
-	translations: Translations
+  translations: Translations
 }
 
 const SectionWhatIs: React.FC<SectionWhatIs> = ({ translations }) => {
-	return (
-		<section>
-			<div className="flex flex-col 2xl:flex-row xl:flex-row lg:flex-row">
-				<div className="flex-1 flex items-center justify-center order-2 2xl:xl:lg:order-1">
-					<Image
-						src={"images/what-is-hologram.svg"}
-						alt={"what is hologram"}
-						width={395}
-						height={550}
-						className="w-[395px] h-[550px]"
-						priority={true}
-					/>
-				</div>
-				<div className="flex-1 text-justify 2xl:px-20 xl:px-20 lg:px-20 order-1 2xlorder-2 :xl:order-2 lg:order-2">
-					<p
-						className="
+  return (
+    <section>
+      <div className="flex flex-col 2xl:flex-row xl:flex-row lg:flex-row">
+        <div className="flex-1 flex items-center justify-center order-2 2xl:xl:lg:order-1">
+          <Image
+            src={'images/what-is-hologram.svg'}
+            alt={'what is hologram'}
+            width={395}
+            height={550}
+            className="w-[395px] h-[550px]"
+            priority={true}
+          />
+        </div>
+        <div className="flex-1 text-justify 2xl:px-20 xl:px-20 lg:px-20 order-1 2xlorder-2 :xl:order-2 lg:order-2">
+          <p
+            className="
 							mb-6
 							2xl:pt-20
 							xl:pt-20
@@ -36,16 +37,15 @@ const SectionWhatIs: React.FC<SectionWhatIs> = ({ translations }) => {
 							text-4xl
 							2xl:text-5xl
 							xl:text-5xl
-							lg:text-5xl">
-						{translations?.what_is_title}
-					</p>
-					<p>
-						{translations?.what_is}
-					</p>
-				</div>
-			</div>
-		</section>
-	);
+							lg:text-5xl"
+          >
+            {translations?.what_is_title}
+          </p>
+          <p>{translations?.what_is}</p>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default React.memo(SectionWhatIs);
+export default React.memo(SectionWhatIs)
