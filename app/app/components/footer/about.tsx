988d4cf684a924be8048ea4f0f46e980913428a6
SergioAR <alonsoromansergio@gmail.com>
2024-09-18 15:18:00 -0600

    feat:Navigation bar integrations and minor changes to the footer and about section

--- app/app/components/footer/about.tsx
+++ app/app/components/footer/about.tsx
@@ -11,18 +11,30 @@ interface About {
 
 const About: React.FC<About> = ({ translations }) => {
   return (
-    <div className="lg:p-4 lg:flex lg:space-x-4 lg:flex-1 items-center text-center text-gray-500">
+    <div
+      className="
+        lg:p-4
+        lg:flex
+        order-2
+        lg:order-1
+        xl:order-1
+        2xl:order-1
+        lg:space-x-4
+        lg:flex-1
+        items-center
+        text-center
+        text-gray-500">
+      <div className="p-4">
+        <div className="flex justify-center space-x-7 lg:space-x-4 xl:space-x-4 2xl:space-x-4">
+          <Image src={'images/ico-x.svg'} alt={'x'} width={26} height={26} className="f w-[30px] h-[30px]" />
+          <Image src={'images/ico-in.svg'} alt={'in'} width={26} height={26} className="f w-[30px] h-[30px]" />
+          <Image src={'images/ico-github.svg'} alt={'github'} width={26} height={26} className="f w-[30px] h-[30px]" />
+          <Image src={'images/ico-hologram.svg'} alt={'hologram'} width={26} height={26} className="f w-[30px] h-[30px]" />
+        </div>
+      </div>
       <div className="p-4 text-center justify-center">{translations.copy_right}</div>
       <div className="lg:mb-0 hidden lg:block">
         <Image src={'images/separation.svg'} alt="separation" width={1} height={42} />
-      </div>
-      <div className="p-4">
-        <div className="flex space-x-4">
-          <Image src={'images/ico-x.svg'} alt={'x'} width={26} height={26} className="flex-1" />
-          <Image src={'images/ico-in.svg'} alt={'in'} width={26} height={26} className="flex-1" />
-          <Image src={'images/ico-github.svg'} alt={'github'} width={26} height={26} className="flex-1" />
-          <Image src={'images/ico-hologram.svg'} alt={'hologram'} width={26} height={26} className="flex-1" />
-        </div>
       </div>
     </div>
   )
