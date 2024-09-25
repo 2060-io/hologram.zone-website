f39cd9923081b25268ed6e84fbef351f0bf6c964
Andres Vallecilla <andresfelipe083195@hotmail.com>
2024-09-25 10:09:02 -0500

    fix: change name in favicon

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -18,9 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-white dark:bg-gray-900">
-      <head>
-        <link rel="icon" type="image/png" href="images/ico-hologram.ico"></link>
-      </head>
       <body className={`font-euclidCircularA ` + inter.className}>{children}</body>
     </html>
   )
