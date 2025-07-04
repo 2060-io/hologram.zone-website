fa78b23590e4e59fe4d49c497613ed83704dc4ad
Mohammad Abubakr <47004492+mabubakr1113@users.noreply.github.com>
2025-07-04 18:34:53 +0500

    fix: Bottom icon alignment (#60)
    Co-authored-by: mabubakr1113 <m.abuabkr3758@gmail.com>

--- app/app/components/sections/sectionStandardsBuilt.tsx
+++ app/app/components/sections/sectionStandardsBuilt.tsx
@@ -10,6 +10,51 @@ interface SectionStandardsBuilt {
   translations: Translations
 }
 
+const logos = [
+  {
+    href: 'https://identity.foundation/didcomm-messaging/spec/',
+    src: '/images/logo-didcomm.svg',
+    alt: 'didcomm',
+    width: 204,
+    height: 50,
+  },
+  {
+    href: 'https://www.w3.org/',
+    src: '/images/logo-w3c.svg',
+    alt: 'w3c',
+    width: 105,
+    height: 50,
+  },
+  {
+    href: 'https://github.com/openwallet-foundation/credo-ts',
+    src: '/images/logo-credo.svg',
+    alt: 'credo',
+    width: 71,
+    height: 50,
+  },
+  {
+    href: 'https://identity.foundation/',
+    src: '/images/logo-dif.png',
+    alt: 'dif',
+    width: 148,
+    height: 50,
+  },
+  {
+    href: 'https://trustoverip.org/',
+    src: '/images/logo-trust-over-ip.svg',
+    alt: 'trust over ip',
+    width: 120,
+    height: 50,
+  },
+  {
+    href: 'https://openwallet.foundation/',
+    src: '/images/logo-open-wallet.svg',
+    alt: 'open wallet',
+    width: 196,
+    height: 50,
+  },
+]
+
 const SectionStandardsBuilt: React.FC<SectionStandardsBuilt> = ({ translations }) => {
   return (
     <div>
@@ -18,127 +63,26 @@ const SectionStandardsBuilt: React.FC<SectionStandardsBuilt> = ({ translations }
           {translations.built_standar}
         </div>
       </div>
-      <div className="bg-white">
-        <div className="container mx-auto 2xl:px-28 xl:px-28 lg:px-28 px-6 lg:flex xl:flex 2xl:flex items-center justify-center pt-5 pb-7">
-          <div className="flex lg:flex-1 xl:flex-1 2xl:flex-1 items-center">
-            <div className="flex-1 mx-auto">
-              <Link
-                href="https://identity.foundation/didcomm-messaging/spec/"
-                target="_blank"
-                className="text-center"
-              >
-                <Image
-                  src={'images/logo-didcomm.svg'}
-                  alt="didcomm"
-                  width={204}
-                  height={60}
-                  priority={false}
-                  className="mx-auto p-0 -mb-6 transform transition duration-300 ease-in-out hover:scale-110"
-                />
-              </Link>
-            </div>
-            <div className="flex-1">
-              <Link href="https://www.w3.org/" target="_blank">
-                <Image
-                  src={'images/logo-w3c.svg'}
-                  alt="w3c"
-                  width={105}
-                  height={50}
-                  priority={false}
-                  className="mx-auto pt-3 transition duration-300 ease-in-out hover:scale-110"
-                />
-              </Link>
-            </div>
-          </div>
 
-          <div className="flex lg:flex-1 xl:flex-1 2xl:flex-1 my-5">
-            <div className="flex-1 lg:hidden xl:hidden 2xl:hidden">
-              <Link href="https://openwallet.foundation/" target="_blank">
+      <div className="bg-white min-h-[150px] flex items-center">
+        <div
+          className="container mx-auto 2xl:px-28 xl:px-28 lg:px-28 px-6 py-8 
+                        grid grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-10 xl:gap-14 2xl:gap-20
+                        lg:flex lg:flex-wrap lg:justify-center lg:items-center"
+        >
+          {logos.map(logo => (
+            <Link key={logo.alt} href={logo.href} target="_blank" rel="noopener noreferrer">
+              <div className="h-[50px]  flex items-center justify-center">
                 <Image
-                  src={'images/logo-open-wallet.svg'}
-                  alt="open wallet"
-                  width={196}
-                  height={50}
-                  priority={false}
-                  className="mx-auto pt-4 pr-4 transition duration-300 ease-in-out hover:scale-110"
+                  src={logo.src}
+                  alt={logo.alt}
+                  width={logo.width}
+                  height={logo.height}
+                  className="h-[50px] w-auto object-contain transition-transform duration-300 ease-in-out hover:scale-110"
                 />
-              </Link>
-            </div>
-            <div className="flex-1 hidden lg:block xl:block 2xl:block">
-              <Link href="https://github.com/openwallet-foundation/credo-ts" target="_blank">
-                <Image
-                  src={'images/logo-credo.svg'}
-                  alt="credo"
-                  width={71}
-                  height={50}
-                  priority={false}
-                  className="mx-auto pt-3 transition duration-300 ease-in-out hover:scale-110"
-                />
-              </Link>
-            </div>
-            <div className="flex-1 block lg:hidden xl:hidden 2xl:hidden">
-              <Link href="https://github.com/openwallet-foundation/credo-ts" target="_blank">
-                <Image
-                  src={'images/logo-credo.svg'}
-                  alt="credo"
-                  width={71}
-                  height={50}
-                  priority={false}
-                  className="mx-auto pt-3 transition duration-300 ease-in-out hover:scale-110"
-                />
-              </Link>
-            </div>
-            <div className="flex-1 hidden lg:block xl:block 2xl:block">
-              <Link href="https://identity.foundation/" target="_blank">
-                <Image
-                  src={'images/logo-dif.png'}
-                  alt="dif"
-                  width={148}
-                  height={50}
-                  priority={false}
-                  className="mx-auto pt-3 pl-4 transition duration-300 ease-in-out hover:scale-110"
-                />
-              </Link>
-            </div>
-          </div>
-          <div className="flex lg:flex-1 xl:flex-1 2xl:flex-1">
-            <div className="flex-1">
-              <Link href="https://trustoverip.org/" target="_blank">
-                <Image
-                  src={'images/logo-trust-over-ip.svg'}
-                  alt="trust over ip"
-                  width={120}
-                  height={50}
-                  priority={false}
-                  className="mx-auto pt-3 px-2 transition duration-300 ease-in-out hover:scale-110"
-                />
-              </Link>
-            </div>
-            <div className="flex-1 hidden lg:block xl:block 2xl:block">
-              <Link href="https://openwallet.foundation/" target="_blank">
-                <Image
-                  src={'images/logo-open-wallet.svg'}
-                  alt="open wallet"
-                  width={196}
-                  height={50}
-                  priority={false}
-                  className="mx-auto pt-4 pr-4 transition duration-300 ease-in-out hover:scale-110"
-                />
-              </Link>
-            </div>
-            <div className="flex-1 block lg:hidden xl:hidden 2xl:hidden">
-              <Link href="https://identity.foundation/" target="_blank">
-                <Image
-                  src={'images/logo-dif.png'}
-                  alt="dif"
-                  width={148}
-                  height={50}
-                  priority={false}
-                  className="mx-auto pt-3 pl-4 transition duration-300 ease-in-out hover:scale-110"
-                />
-              </Link>
-            </div>
-          </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
