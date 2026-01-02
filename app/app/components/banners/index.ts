ac7ea40e54f22eb25e655b4d314cb4ffa2d35a6a
AirKyzzZ <airkyzzz.jiji@gmail.com>
2026-01-02 12:31:46 +0100

    chore: address reviewer feedback - remove lock files from gitignore, delete app/ and node_modules from tracking

--- app/app/components/banners/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BannerDownloadHolomgram } from './bannerDownloadHologram'
-export { default as BannerHologramMessaging } from './bannerHologramMessaging'
-export { default as bannerServiceHologram } from './bannerServiceHologram'
--- app/app/components/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './header'
-export { default as QRCodeWithLogo } from './QRCodeWithLogo'
-export * from './types'
-export { default as useDeviceDetect } from './useDeviceDetect'
