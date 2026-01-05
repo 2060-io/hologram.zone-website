ee8f2adeab5f2f3256b743978ff4c18724e28e73
Maxime Mansiet <airkyzzz.jiji@gmail.com>
2026-01-05 19:39:30 +0100

    chore: cleanup gitignore and build artifacts (#90)

--- app/app/components/navBars/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as NavbarFooter } from './navbarFooter'
-export { default as NavBarTopPage } from './navbarTopPage'
--- app/app/components/sections/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SectionProofOfTrust } from './sectionProofOfTrust'
-export { default as SectionStandardsBuilt } from './sectionStandardsBuilt'
-export { default as SectionWhatIs } from './sectionWhatIs'
